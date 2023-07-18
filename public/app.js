e411829d8a605dcb46639bc1ad65b8e7bb91a4e9
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-07-18 15:39:01 +0530

    Continuous calling of api

--- public/app.js
+++ public/app.js
@@ -28,21 +28,61 @@ function Onsend(e) {
     });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  axios
-    .get("http://localhost:3000/message")
-    .then((res) => {
-      console.log(res);
-      res.data.forEach((element) => {
-        const li = document.createElement("li");
-        li.setAttribute("id", "1");
-        li.appendChild(
-          document.createTextNode(`${element.name}: ${element.text}`)
-        );
+// window.addEventListener("DOMContentLoaded", () => {
+//   axios
+//     .get("http://localhost:3000/message")
+//     .then((res) => {
+//       console.log(res);
+//       res.data.forEach((element) => {
+//         const li = document.createElement("li");
+//         li.setAttribute("id", "1");
+//         li.appendChild(
+//           document.createTextNode(`${element.name}: ${element.text}`)
+//         );
 
-        const chats = document.getElementById("chats");
-        chats.appendChild(li);
-      });
+//         const chats = document.getElementById("chats");
+//         chats.appendChild(li);
+//       });
+//     })
+//     .catch();
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  previousData = [];
+
+  function compareData(data) {
+    if (JSON.stringify(data) !== JSON.stringify(previousData)) {
+       const chats = document.getElementById("chats");
+       chats.innerHTML = ""; 
+       
+       data.forEach((element) => {
+         const li = document.createElement("li");
+         li.setAttribute("id", "1");
+         li.appendChild(
+           document.createTextNode(`${element.name}: ${element.text}`)
+           )
+           
+           chats.appendChild(li);
+          });
+          previousData = data;
+
+    }
+  }
+
+  function fetchdata() {
+    axios.get("http://localhost:3000/message")
+    .then((res)=>{
+      console.log(res);
+      compareData(res.data);
     })
-    .catch();
+    .catch((error)=>{
+      console.log(error)
+    })
+
+  }
+
+  fetchdata()
+
+  setInterval(fetchdata,1000)
+
 });
