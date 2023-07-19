b19718e9abcfbc5b9dfe27901d7e0f864c29d0b1
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-07-20 01:05:38 +0530

    optimisation (caht  rendring from localStorage)

--- public/app.js
+++ public/app.js
@@ -28,61 +28,63 @@ function Onsend(e) {
     });
 }
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   axios
-//     .get("http://localhost:3000/message")
-//     .then((res) => {
-//       console.log(res);
-//       res.data.forEach((element) => {
-//         const li = document.createElement("li");
-//         li.setAttribute("id", "1");
-//         li.appendChild(
-//           document.createTextNode(`${element.name}: ${element.text}`)
-//         );
+window.addEventListener("DOMContentLoaded", (req, res, next) => {
+   previousdata = JSON.parse(localStorage.getItem("messages"));
+  if (!previousdata || previousdata.length === 0) {
+    axios
+      .get("http://localhost:3000/message?lastidinlocalstorage=undefined")
+      .then((result) => {
+        const data = result.data.slice(0, 10);
+        //console.log(data)
+        data.forEach((element) => {
+          const chats = document.getElementById("chats");
+          const li = document.createElement("li");
+          li.setAttribute("id", "1");
+          li.appendChild(
+            document.createTextNode(`${element.name}: ${element.text}`)
+          );
+          chats.appendChild(li);
+          previousdata = localStorage.setItem("messages", JSON.stringify(data));
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    previousdata = JSON.parse(localStorage.getItem("messages"));
+    // console.log(previousdata)
+    const lastidinlocalstorage = previousdata[previousdata.length - 1].id;
+    // console.log("lastIdInLocalStorage:", lastidinlocalstorage);
 
-//         const chats = document.getElementById("chats");
-//         chats.appendChild(li);
-//       });
-//     })
-//     .catch();
-// });
+    axios
+      .get(
+        `http://localhost:3000/message?lastidinlocalstorage=${lastidinlocalstorage}`
+      )
+      .then((result) => {
+        const data = result.data;
+        console.log(data);
 
-window.addEventListener("DOMContentLoaded", () => {
-  previousData = [];
+        previousdata = [...data.reverse(), ...previousdata.reverse()];
+       previousdata=previousdata.slice(0,10)
+       console.log(typeof(previousdata))
 
-  function compareData(data) {
-    if (JSON.stringify(data) !== JSON.stringify(previousData)) {
-       const chats = document.getElementById("chats");
-       chats.innerHTML = ""; 
-       
-       data.forEach((element) => {
-         const li = document.createElement("li");
-         li.setAttribute("id", "1");
-         li.appendChild(
-           document.createTextNode(`${element.name}: ${element.text}`)
-           )
-           
-           chats.appendChild(li);
-          });
-          previousData = data;
+        //localStorage.setItem("messages",JSON.stringify(previousdata))
 
-    }
+        // console.log(previousdata);
+        previousdata.forEach((element) => {
+          const chats = document.getElementById("chats");
+          const li2 = document.createElement("li");
+          li2.setAttribute("id", "2");
+          li2.appendChild(
+            document.createTextNode(`${element.name}: ${element.text}`)
+          );
+          chats.appendChild(li2);
+        });
+        //localStorage.setItem("messages", JSON.stringify(previousdata));
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-  function fetchdata() {
-    axios.get("http://localhost:3000/message")
-    .then((res)=>{
-      console.log(res);
-      compareData(res.data);
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-
-  }
-
-  fetchdata()
-
-  setInterval(fetchdata,1000)
-
 });
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sequelize = require("../config/database");
+const Op = require("sequelize");
 const user = require("../models/messages");
 
 const bcrypt = require("bcrypt");
@@ -8,15 +9,14 @@ const jwt = require("jsonwebtoken");
 const UserAuthentication = require("../middleware/auth");
 
 router.post("/", UserAuthentication.authenticate, (req, res, next) => {
-console.log("bbbboooooddddyyy",req.user)
+  console.log("bbbboooooddddyyy", req.user);
 
   const text = req.body.text;
-  const name=req.user.name
-  
+  const name = req.user.name;
 
   user
     .create({
-      name:name,
+      name: name,
       text: text,
       UserInfoId: req.user.id,
     })
@@ -28,14 +28,29 @@ console.log("bbbboooooddddyyy",req.user)
     });
 });
 
-router.get("/", (req, res, next)=>{
-  user.findAll()
-  .then((data)=>{
-    res.send(data)
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
+router.get("/", (req, res, next) => {
+  const lastid = req.query.lastidinlocalstorage;
+  console.log(lastid)
+
+  if (lastid == undefined) {
+    user
+      .findAll()
+      .then((alldata) => {
+        res.send(alldata);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    user
+      .findAll()
+      .then((data) => {
+        res.send(data.slice(lastid));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
 module.exports = router;
